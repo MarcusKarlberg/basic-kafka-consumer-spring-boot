b11b09de88facf930e1184c71e424cde16a58312
markar7 <marcus.karlberg@knowit.se>
2025-08-06 10:44:30 +0200

    modified kafka config and finished testProductCreatedEventHandler

--- src/main/java/se/marcuskarlberg/EmailNotificationService/config/KafkaConsumerConfig.java
+++ src/main/java/se/marcuskarlberg/EmailNotificationService/config/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -24,56 +25,52 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
-  private final Environment environment;
-
-  public KafkaConsumerConfig(Environment environment) {
-    this.environment = environment;
-  }
+  @Autowired
+  Environment environment;
 
   @Bean
-  public ConsumerFactory<String, String> consumerFactory() {
+  ConsumerFactory<String, Object> consumerFactory() {
     Map<String, Object> config = new HashMap<>();
     config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("spring.kafka.consumer.bootstrap-servers"));
     config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-    //ErrorHandlingDeserializer to handle errors gracefully.
-    config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class); //Catch any deserializer exceptions
-    config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class); //The actual deserialization should be done using JsonDeserializer
-
-    config.put(JsonDeserializer.TRUSTED_PACKAGES, environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
+    config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+    config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+    config.put(JsonDeserializer.TRUSTED_PACKAGES,
+      environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
     config.put(ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("consumer.group-id"));
+    config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, environment.getProperty("spring.kafka.consumer.auto-offset-reset"));
+
     return new DefaultKafkaConsumerFactory<>(config);
   }
 
   @Bean
-  ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
-    ConsumerFactory<String, String> consumerFactory, KafkaTemplate<String, String> kafkaTemplate) {
-    //Send errors to Dead Letter Topic - 3 retries, 5 s interval.
-    DefaultErrorHandler errorHandler = new DefaultErrorHandler(
-      new DeadLetterPublishingRecoverer(kafkaTemplate),
-      new FixedBackOff(5000,3)
-    );
-    errorHandler.addRetryableExceptions(RetriableException.class);
-    errorHandler.addNotRetryableExceptions(NotRetriableException.class);
+  ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+    ConsumerFactory<String, Object> consumerFactory, KafkaTemplate<String, Object> kafkaTemplate) {
 
-    ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    DefaultErrorHandler errorHandler = new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate),
+      new FixedBackOff(5000,3));
+    errorHandler.addNotRetryableExceptions(NotRetriableException.class);
+    errorHandler.addRetryableExceptions(RetriableException.class);
+
+    ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
     factory.setConsumerFactory(consumerFactory);
     factory.setCommonErrorHandler(errorHandler);
+
     return factory;
   }
 
-  //This is used to errors to Dead Letter Topics
   @Bean
-  KafkaTemplate<String, String> createKafkaTemplate(ProducerFactory producerFactory) {
+  KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
     return new KafkaTemplate<>(producerFactory);
   }
 
-  //This is used to errors to Dead Letter Topics
   @Bean
-  ProducerFactory<String, String> producerFactory() {
+  ProducerFactory<String, Object> producerFactory() {
     Map<String, Object> config = new HashMap<>();
     config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("spring.kafka.consumer.bootstrap-servers"));
     config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
     config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
     return new DefaultKafkaProducerFactory<>(config);
   }
 }
--- src/test/java/se/marcuskarlberg/EmailNotificationService/handler/ProductCreatedEventHandlerIntegrationTest.java
+++ src/test/java/se/marcuskarlberg/EmailNotificationService/handler/ProductCreatedEventHandlerIntegrationTest.java
@@ -2,23 +2,51 @@ package se.marcuskarlberg.EmailNotificationService.handler;
 
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.*;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.test.context.*;
+import org.springframework.web.client.RestTemplate;
+import se.marcuskarlberg.EmailNotificationService.entity.ProcessedEvent;
+import se.marcuskarlberg.EmailNotificationService.repository.ProcessedEventRepository;
 import se.marcuskarlberg.core.ProductCreatedEvent;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 import static se.marcuskarlberg.EmailNotificationService.handler.ProductCreatedEventHandler.TOPIC_NAME;
 
 @EmbeddedKafka //To start embedded kafka server
 @SpringBootTest(properties="spring.kafka.consumer.bootstrap-servers=${spring.embedded.kafka.brokers}")
 public class ProductCreatedEventHandlerIntegrationTest {
 
+  @MockBean
+  ProcessedEventRepository processedEventRepository;
+
+  @MockBean
+  RestTemplate restTemplate;
+
+  @Autowired
+  KafkaTemplate<String, Object> kafkaTemplate;
+
+  @SpyBean
+  ProductCreatedEventHandler eventHandler;
+
+  @Autowired
+  private ProductCreatedEventHandler productCreatedEventHandler;
+
   @Test
-  void testProductCreatedEventHandler() {
+  void testProductCreatedEventHandler() throws ExecutionException, InterruptedException {
     ProductCreatedEvent productCreatedEvent = ProductCreatedEvent.builder()
       .title("test title")
       .price(BigDecimal.ONE)
@@ -29,12 +57,36 @@ public class ProductCreatedEventHandlerIntegrationTest {
     String messageId = UUID.randomUUID().toString();
     String messageKey = productCreatedEvent.getProductId();
 
-    ProducerRecord<String, ProductCreatedEvent> record =
+    ProducerRecord<String, Object> record =
       new ProducerRecord<>(TOPIC_NAME, messageKey, productCreatedEvent);
 
     record.headers().add("messageId", messageId.getBytes());
     record.headers().add(KafkaHeaders.RECEIVED_KEY, messageKey.getBytes());
 
-    //TODO: act and assertions
+    when(processedEventRepository.findByMessageId(anyString()))
+      .thenReturn(Optional.of(new ProcessedEvent(UUID.randomUUID().toString(), UUID.randomUUID().toString())));
+    when(processedEventRepository.save(any())).thenReturn(null);
+
+    String responseBody = "{\"key\":\"value\"}";
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    ResponseEntity<String> responseEntity = new ResponseEntity<>(responseBody, headers, HttpStatus.OK);
+
+    when(restTemplate.exchange(any(String.class), any(HttpMethod.class), isNull(), eq(String.class)))
+      .thenReturn(responseEntity);
+
+    kafkaTemplate.send(record).get();
+
+    ArgumentCaptor<String> messageIdCaptor = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor<String> messageKeyCaptor = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor<ProductCreatedEvent> eventCaptor = ArgumentCaptor.forClass(ProductCreatedEvent.class);
+
+    verify(productCreatedEventHandler, timeout(5000).times(1)).handler(eventCaptor.capture(),
+      messageIdCaptor.capture(),
+      messageKeyCaptor.capture());
+
+    assertEquals(messageId, messageIdCaptor.getValue());
+    assertEquals(messageKey, messageKeyCaptor.getValue());
+    assertEquals(productCreatedEvent.getProductId(), eventCaptor.getValue().getProductId());
   }
 }
